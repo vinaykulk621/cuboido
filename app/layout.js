4e8d459f0b37de94e46756825d4905d7a5ca5d1d
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-04-20 17:58:50 +0530

    Anal ytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Cuboido",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="overflow-hidden bg-black">{children}</body>
+      <body className="overflow-hidden bg-black">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
